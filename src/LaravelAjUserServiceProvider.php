c069dc811cb8d3b8e60fc4f3adaaa716e485ed0f
SharathSkN <sharath@ajency.in>
2017-09-08 00:14:48 +0530

    Bug fix - Class Name

--- src/LaravelAjUserServiceProvider.php
+++ src/LaravelAjUserServiceProvider.php
@@ -1,18 +1,16 @@
 <?php
 
-namespace Ajency\Auth;
+namespace Ajency\User;
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelAjUserProvider extends ServiceProvider
-{
+class LaravelAjUserServiceProvider extends ServiceProvider {
     /**
      * Bootstrap the application services.
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
         
         // $this->loadRoutesFrom(__DIR__.'/routes/web.php');
@@ -24,7 +22,7 @@ class LaravelAjUserProvider extends ServiceProvider
             __DIR__.'/config/aj_user_migrations.php' => config_path('aj_user_migrations.php'),
 
             /* Controller files shifted */
-            __DIR__.'/Controllers/SocialAuthController.php' => public_path('app/Http/Controllers/Ajency/Auth/SocialAuthController.php'),
+            __DIR__.'/Controllers/SocialAuthController.php' => public_path('app/Http/Controllers/Ajency/User/SocialAuthController.php'),
 
             /*__DIR__.'/Models/SocialAccountService.php' => public_path('app/SocialAccountService.php'),*/
 
@@ -36,8 +34,8 @@ class LaravelAjUserProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         //
+        dd("Package successfully mapped");
     }
 }
